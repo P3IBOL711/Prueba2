b1d2e4826881b027d37369f34cef790bf1af3b92
Pablo <pablofol2003@gmail.com>
2024-04-27 22:24:53 +0200

    kjkkkk

--- src/dungeongen.js
+++ src/dungeongen.js
@@ -54,6 +54,27 @@ export default class Dungeongen {
 
     init() {
 
+        let ar_e1 = {
+
+            name: "ar_e1",
+            
+            level: "armory",
+            
+            path: "../../assets/armory/ar_e1.tmx",
+            
+            entrance: true,
+            exit: false,
+            empty: false, //Habitacion vacia
+            deadend: false,//para saber que solo tiene una salida
+            shop:false,
+            
+            door_north: true,
+            door_south: true,
+            door_east: true,
+            door_west: true
+            
+            };
+
         //Inicializacion
         let dungeon = [];
 
@@ -91,27 +112,27 @@ export default class Dungeongen {
         entranceX = Math.floor(Math.random() * (N - 2)) + 1;
         entranceY = Math.floor(Math.random() * (M - 2)) + 1;
 
+        dungeo[entranceY][entranceX] = ar_e1;
+        // dungeon[entranceY][entranceX] = {
 
-        dungeon[entranceY][entranceX] = {
+        //     name: "ar_e1",
 
-            name: "ar_e1",
+        //     level: "armory",
 
-            level: "armory",
+        //     path: "../../assets/armory/ar_e1.tmx",
 
-            path: "../../assets/armory/ar_e1.tmx",
+        //     entrance: true,
+        //     exit: false,
+        //     empty: false, //Habitacion vacia
+        //     deadend: false,//para saber que solo tiene una salida
+        //     shop: false,
 
-            entrance: true,
-            exit: false,
-            empty: false, //Habitacion vacia
-            deadend: false,//para saber que solo tiene una salida
-            shop: false,
+        //     door_north: true,
+        //     door_south: true,
+        //     door_east: true,
+        //     door_west: true
 
-            door_north: true,
-            door_south: true,
-            door_east: true,
-            door_west: true
-
-        }; //Asignamos la entrada
+        // }; //Asignamos la entrada
 
         console.log(`Entrada generada en la casilla: ${entranceY}, ${entranceX}`);
 
